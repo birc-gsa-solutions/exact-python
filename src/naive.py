8e0e170194885376cfc21296f0bbc7b2f0272469
Thomas Mailund <mailund@birc.au.dk>
2022-01-27 12:53:56 +0100

    project setup -- taylored to python

---
+++ src/naive.py
@@ -0,0 +1,17 @@
+"""Implementation of the naive exact matching algorithm."""
+
+import argparse
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description="Exact matching using the naive method")
+    argparser.add_argument("genome", type=argparse.FileType('r'))
+    argparser.add_argument("reads", type=argparse.FileType('r'))
+    args = argparser.parse_args()
+    print(f"Find every reads in {args.reads.name} " +
+          f"in genome {args.genome.name}")
+
+
+if __name__ == '__main__':
+    main()
